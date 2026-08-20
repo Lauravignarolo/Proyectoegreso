e99f188d4105877f12aadcc0abafdafe3a5f5378
Lauravignarolo <Lauravignarolo@gmail.com>
2026-08-20 11:24:08 -0300

    agregando paginas

--- config/config.php
+++ config/config.php
@@ -1,22 +1,13 @@
 
 <?php
+//Guarda como constante el directorio padre de config, es decir proyecto
+define("RUTA_RAIZ", dirname(__DIR__));
+define("RUTA_APP", RUTA_RAIZ . "/app");
+define("RUTA_MODELO", RUTA_APP . "/modelo");
+define("RUTA_CONTROLADOR", RUTA_APP . "/controlador");
+define("RUTA_VISTA", RUTA_APP . "/vista");
+define("RUTA_PUBLIC", RUTA_RAIZ . "/public");
 
-require_once __DIR__ . '/../clases/ConectorPDO.php';
+//Se cargan las herramientas para generar las variables de entorno
+require_once RUTA_RAIZ . "/vendor/autoload.php";
 
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
-$baseDatos = "proyectoegreso";
-
-$conexionBD = new ConectorPDO(
-    $servidor,
-    $usuario,
-    $password,
-    $baseDatos
-);
-
-$conexion = $conexionBD->establecerConexion();
-
-if ($conexion === null) {
-    die("Error: no se pudo conectar a la base de datos.");
-}
